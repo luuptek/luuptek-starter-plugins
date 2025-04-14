7b1e52a2bd638fae41495ee8470896c5ca62c474
Timo L <timo@luuptek.fi>
2025-04-14 09:07:23 +0300

    Possibly disable email from feature

--- plugins/email-from.php
+++ plugins/email-from.php
@@ -4,13 +4,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+// Possibly disable this feature
+if ( apply_filters( 'disable_luuptek_starter_email_from', false ) ) {
+	return;
+}
+
 /**
  * Change mail_from_name to current site name.
  */
 add_filter(
 	'wp_mail_from_name',
 	function ( $name ) {
-
 		// If set elsewhere to anything else than default fallback "WordPress".
 		if ( 'WordPress' !== $name ) {
 			return $name;
