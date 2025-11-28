91c15fde4eb4792fe8a88aafcb360a5c1d65e8fe
Timo L <timo@luuptek.fi>
2025-11-28 16:08:53 +0200

    Disable password changed email to admin

--- plugins/email.php
+++ plugins/email.php
@@ -6,3 +6,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Disable password changed notification email scam
 add_filter( 'send_password_change_email', '__return_false' );
+
+// Disable password changed email to admin
+if ( ! function_exists( 'wp_password_change_notification' ) ) {
+	function wp_password_change_notification( $user ) {
+		// Do nothing — disables admin notification email.
+	}
+}
