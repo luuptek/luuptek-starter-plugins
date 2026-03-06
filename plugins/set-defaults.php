64e7dc4b87aa02b36068ea6db1f02e446a5fe9c5
Timo L <timo@luuptek.fi>
2026-03-06 17:37:55 +0200

    Remove legacy ping sites default

--- plugins/set-defaults.php
+++ plugins/set-defaults.php
@@ -28,7 +28,6 @@ add_action(
 			'date_format'                   => 'd.m.Y',
 			'default_post_edit_rows'        => 40,
 			'permalink_structure'           => '/%postname%/',
-			'ping_sites'                    => "http://rpc.pingomatic.com \n http://rpc.twingly.com \n http://api.feedster.com/ping \n http://api.moreover.com/RPC2 \n http://api.moreover.com/ping \n http://www.blogdigger.com/RPC2 \n http://www.blogshares.com/rpc.php \n http://www.blogsnow.com/ping \n http://www.blogstreet.com/xrbin/xmlrpc.cgi \n http://bulkfeeds.net/rpc \n http://www.newsisfree.com/xmlrpctest.php \n http://ping.blo.gs/ \n http://ping.feedburner.com \n http://ping.syndic8.com/xmlrpc.php \n http://ping.weblogalot.com/rpc.php \n http://rpc.blogrolling.com/pinger/ \n http://rpc.technorati.com/rpc/ping \n http://rpc.weblogs.com/RPC2 \n http://www.feedsubmitter.com \n http://blo.gs/ping.php \n http://www.pingerati.net \n http://www.pingmyblog.com \n http://geourl.org/ping \n http://ipings.com \n http://www.weblogalot.com/ping",
 		];
 
 		// Update options to wpdp
