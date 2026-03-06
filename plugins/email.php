69b746189061a5665540d1d80370aa09b55959cc
Timo L <timo@luuptek.fi>
2026-03-06 17:57:33 +0200

    Allow user password change emails

--- plugins/email.php
+++ plugins/email.php
@@ -4,9 +4,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// Disable password changed notification email scam
-add_filter( 'send_password_change_email', '__return_false' );
-
 // Disable password changed email to admin
 if ( ! function_exists( 'wp_password_change_notification' ) ) {
 	function wp_password_change_notification( $user ) {
